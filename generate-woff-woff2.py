c983c154e7134574c8fbc08c24b44a6cdd7afd5d
Cristiano Sobral <cssobral2013@gmail.com>
2025-05-25 15:18:04 -0300

    Update

---
+++ generate-woff-woff2.py
@@ -0,0 +1,333 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/Chamberlain-Thin.ttf'
+woffPath = './web/Chamberlain-Thin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Chamberlain-ThinItalic.ttf'
+woffPath = './web/Chamberlain-ThinItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Chamberlain-ExtraLight.ttf'
+woffPath = './web/Chamberlain-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Chamberlain-ExtraLightItalic.ttf'
+woffPath = './web/Chamberlain-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/Chamberlain-Light.ttf'
+woffPath = './web/Chamberlain-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Chamberlain-LightItalic.ttf'
+woffPath = './web/Chamberlain-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Chamberlain-LightItalic.ttf'
+woffPath = './web/Chamberlain-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Chamberlain-Regular.ttf'
+woffPath = './web/Chamberlain-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Chamberlain-Italic.ttf'
+woffPath = './web/Chamberlain-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Chamberlain-Medium.ttf'
+woffPath = './web/Chamberlain-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Chamberlain-MediumItalic.ttf'
+woffPath = './web/Chamberlain-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Chamberlain-SemiBold.ttf'
+woffPath = './web/Chamberlain-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Chamberlain-SemiBoldItalic.ttf'
+woffPath = './web/Chamberlain-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Chamberlain-SemiBold.ttf'
+woffPath = './web/Chamberlain-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Chamberlain-SemiBoldItalic.ttf'
+woffPath = './web/Chamberlain-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Chamberlain-Bold.ttf'
+woffPath = './web/Chamberlain-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Chamberlain-BoldItalic.ttf'
+woffPath = './web/Chamberlain-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Chamberlain-ExtraBold.ttf'
+woffPath = './web/Chamberlain-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Chamberlain-ExtraBoldItalic.ttf'
+woffPath = './web/Chamberlain-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Chamberlain-Black.ttf'
+woffPath = './web/Chamberlain-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Chamberlain-BlackItalic.ttf'
+woffPath = './web/Chamberlain-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/Chamberlain-Thin.ttf'
+woff2Path = './web/Chamberlain-Thin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Chamberlain-ThinItalic.ttf'
+woff2Path = './web/Chamberlain-ThinItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Chamberlain-ExtraLight.ttf'
+woff2Path = './web/Chamberlain-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Chamberlain-ExtraLightItalic.ttf'
+woff2Path = './web/Chamberlain-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/Chamberlain-Light.ttf'
+woff2Path = './web/Chamberlain-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Chamberlain-LightItalic.ttf'
+woff2Path = './web/Chamberlain-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Chamberlain-LightItalic.ttf'
+woff2Path = './web/Chamberlain-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Chamberlain-Regular.ttf'
+woff2Path = './web/Chamberlain-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Chamberlain-Italic.ttf'
+woff2Path = './web/Chamberlain-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Chamberlain-Medium.ttf'
+woff2Path = './web/Chamberlain-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Chamberlain-MediumItalic.ttf'
+woff2Path = './web/Chamberlain-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Chamberlain-SemiBold.ttf'
+woff2Path = './web/Chamberlain-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Chamberlain-SemiBoldItalic.ttf'
+woff2Path = './web/Chamberlain-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Chamberlain-SemiBold.ttf'
+woff2Path = './web/Chamberlain-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Chamberlain-SemiBoldItalic.ttf'
+woff2Path = './web/Chamberlain-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Chamberlain-Bold.ttf'
+woff2Path = './web/Chamberlain-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Chamberlain-BoldItalic.ttf'
+woff2Path = './web/Chamberlain-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Chamberlain-ExtraBold.ttf'
+woff2Path = './web/Chamberlain-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Chamberlain-ExtraBoldItalic.ttf'
+woff2Path = './web/Chamberlain-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Chamberlain-Black.ttf'
+woff2Path = './web/Chamberlain-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Chamberlain-BlackItalic.ttf'
+woff2Path = './web/Chamberlain-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
